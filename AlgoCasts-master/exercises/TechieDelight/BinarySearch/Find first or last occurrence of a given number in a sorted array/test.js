28383983206374bec191b6f66f363682fb555728
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-02-13 07:04:51 -0500

    first and last occurence

--- AlgoCasts-master/exercises/TechieDelight/BinarySearch/Find first or last occurrence of a given number in a sorted array/test.js
+++ AlgoCasts-master/exercises/TechieDelight/BinarySearch/Find first or last occurrence of a given number in a sorted array/test.js
@@ -38,3 +38,22 @@
     Finding last occurrence of the element –
     In order to find the last occurrence of the element, the idea is to modify the normal binary search in such a way that we do not stop the search as soon we find any occurrence of the target element. Instead we update the result to mid and go on searching towards right (towards higher indices) i.e. modify our search space by adjusting low to mid + 1 on finding the target at mid index.
 */
+const { firstOccurence, lastOccurence } = require('./index');
+test('firstOccurence is a function', () => {
+  expect(typeof firstOccurence).toEqual('function');
+});
+test('lastOccurence is a function', () => {
+  expect(typeof lastOccurence).toEqual('function');
+});
+test('First Occurence works properly', () => {
+  expect(firstOccurence([5, 7, 7, 8, 8, 10], 8)).toEqual(3);
+  expect(firstOccurence([5, 7, 7, 8, 8, 10], 5)).toEqual(0);
+  expect(firstOccurence([5, 7, 7, 8, 8, 10], 10)).toEqual(5);
+});
+test('First Occurence works properly', () => {
+  expect(lastOccurence([5, 7, 7, 8, 8, 10], 8)).toEqual(4);
+  expect(lastOccurence([5, 7, 7, 8, 8, 10], 5)).toEqual(0);
+  expect(lastOccurence([5, 7, 7, 8, 8, 10], 10)).toEqual(5);
+  expect(lastOccurence([5, 7, 7, 8, 8, 10], 7)).toEqual(2);
+  expect(lastOccurence([5, 7, 7, 8, 8, 10], 6)).toEqual(-1);
+});
